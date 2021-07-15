1dc107f50aa56974db0297ec5202f2e759881bc8
Jonathan Schneider <jkschneider@gmail.com>
2021-07-15 14:41:39 -0700

    Initial import

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,32 +1,33 @@
+import com.github.jk1.license.LicenseReportExtension
 import nebula.plugin.contacts.Contact
 import nebula.plugin.contacts.ContactsExtension
+import nl.javadude.gradle.plugins.license.LicenseExtension
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.util.*
 
 plugins {
     `java-library`
 
-    id("nebula.release") version "16.0.0"
+    id("org.jetbrains.kotlin.jvm") version "1.5.21"
+    id("nebula.release") version "15.3.1"
 
-    id("nebula.maven-manifest") version "18.4.0"
-    id("nebula.maven-nebula-publish") version "18.4.0"
-    id("nebula.maven-resolved-dependencies") version "18.4.0"
+    id("nebula.maven-manifest") version "17.3.2"
+    id("nebula.maven-nebula-publish") version "17.3.2"
+    id("nebula.maven-resolved-dependencies") version "17.3.2"
 
-    id("nebula.contacts") version "6.0.0"
-    id("nebula.info") version "11.3.3"
+    id("nebula.contacts") version "5.1.0"
+    id("nebula.info") version "9.3.0"
 
-    id("nebula.javadoc-jar") version "18.4.0"
-    id("nebula.source-jar") version "18.4.0"
+    id("nebula.javadoc-jar") version "17.3.2"
+    id("nebula.source-jar") version "17.3.2"
+
+    id("com.google.cloud.artifactregistry.gradle-plugin") version "2.1.1"
 }
 
 apply(plugin = "nebula.publish-verification")
 
 configure<nebula.plugin.release.git.base.ReleasePluginExtension> {
     defaultVersionStrategy = nebula.plugin.release.NetflixOssStrategies.SNAPSHOT(project)
-}
-
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
 }
 
 group = "org.openrewrite.recipe"
@@ -47,23 +48,23 @@ configurations.all {
     }
 }
 
-//The bom version can also be set to a specific version or latest.release.
-val rewriteBomVersion = "latest.release"
+val rewriteVersion = "latest.integration"
 
 dependencies {
     compileOnly("org.projectlombok:lombok:latest.release")
-    compileOnly("com.google.code.findbugs:jsr305:latest.release")
     annotationProcessor("org.projectlombok:lombok:latest.release")
-    implementation(platform("org.openrewrite.recipe:rewrite-recipe-bom:${rewriteBomVersion}"))
 
-    implementation("org.openrewrite:rewrite-java")
-    runtimeOnly("org.openrewrite:rewrite-java-17")
+    implementation("org.openrewrite:rewrite-java:${rewriteVersion}")
+    runtimeOnly("org.openrewrite:rewrite-java-11:${rewriteVersion}")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:latest.release")
     testImplementation("org.junit.jupiter:junit-jupiter-params:latest.release")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:latest.release")
 
-    testImplementation("org.openrewrite:rewrite-test")
+    testImplementation("org.openrewrite:rewrite-test:${rewriteVersion}")
     testImplementation("org.assertj:assertj-core:latest.release")
 }
 
@@ -72,19 +73,31 @@ tasks.named<Test>("test") {
     jvmArgs = listOf("-XX:+UnlockDiagnosticVMOptions", "-XX:+ShowHiddenFrames")
 }
 
-tasks.withType<JavaCompile>().configureEach {
+tasks.named<JavaCompile>("compileJava") {
+    sourceCompatibility = JavaVersion.VERSION_1_8.toString()
+    targetCompatibility = JavaVersion.VERSION_1_8.toString()
+
+    options.isFork = true
+    options.forkOptions.executable = "javac"
+    options.compilerArgs.addAll(listOf("--release", "8"))
     options.encoding = "UTF-8"
     options.compilerArgs.add("-parameters")
-    options.isFork = true
 }
 
-tasks.named<JavaCompile>("compileJava") {
-    options.release.set(8)
+tasks.withType(KotlinCompile::class.java).configureEach {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    doFirst {
+        destinationDir.mkdirs()
+    }
 }
 
 configure<ContactsExtension> {
     val j = Contact("team@moderne.io")
     j.moniker("Team Moderne")
+
     people["team@moderne.io"] = j
 }
 
@@ -99,8 +112,21 @@ configure<PublishingExtension> {
 publishing {
   repositories {
       maven {
-          name = "moderne"
-          url = uri("https://us-west1-maven.pkg.dev/moderne-dev/moderne-recipe")
+          name = "moderne-recipes"
+          url = uri("artifactregistry://us-west1-maven.pkg.dev/moderne-dev/moderne-recipe")
       }
   }
+}
+
+tasks.named("final") {
+  dependsOn(tasks.getByName("publishAllPublicationsToGcpRepository"))
+}
+
+tasks.named("snapshot") {
+  dependsOn(tasks.getByName("publishAllPublicationsToGcpRepository"))
+}
+
+extensions.findByName("buildScan")?.withGroovyBuilder {
+  setProperty("termsOfServiceUrl", "https://gradle.com/terms-of-service")
+  setProperty("termsOfServiceAgree", "yes")
 }
